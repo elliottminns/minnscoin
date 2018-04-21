33f83fb29ed4c70f728c0eae0cf9202a62b1df59
Elliott Minns <elliott.minns@me.com>
2018-04-20 21:25:49 -0500

    Changed POS block to be 500

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -104,7 +104,7 @@ public:
         nPoolMaxTransactions = 3;
         strDarksendPoolDummyAddress = "f25gtaAQYBUNjYrxzZqNdyADSmwPPpWoLC";
         nLastPOWBlock = 500;
-        nPOSStartBlock = 501;
+        nPOSStartBlock = 500;
         nMasternodePaymentStartBlock = 500000000;
     }
 
